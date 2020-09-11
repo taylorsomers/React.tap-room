7f1ddac60074ddbd3979d83f24d8a1984fab9539
Taylor Somers <tsomers@yandex.com>
2020-09-11 14:58:49 -0400

    Refactor Keg component to receive whenKegClicked function as prop

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,11 +4,13 @@ import PropTypes from "prop-types";
 function Keg(props){
   return (
     <React.Fragment>
-      <h1>{props.kegName}</h1>
-      <h2>{props.brewery}</h2>
-      <p>{props.price}</p>
-      <p>{props.alcoholContent}%</p>
-      <p>{props.pints} Pints Remaining</p>
+      <div onClick = {() => props.whenKegClicked(props.id)}>
+        <h1>{props.kegName}</h1>
+        <h2>{props.brewery}</h2>
+        <p>{props.price}</p>
+        <p>{props.alcoholContent}%</p>
+        <p>{props.pints} Pints Remaining</p>
+      </div>
     </React.Fragment>
   );
 }
@@ -18,7 +20,9 @@ Keg.propTypes = {
   brewery: PropTypes.string,
   price: PropTypes.string,
   alcoholContent: PropTypes.number,
-  pints: PropTypes.number
+  pints: PropTypes.number,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
 };
 
 export default Keg;
