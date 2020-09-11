2835c1e880efc6361ba4da392a2246961a9195f0
Taylor Somers <tsomers@yandex.com>
2020-09-11 13:58:11 -0400

    Add masterKegList to state

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -6,7 +6,8 @@ class KegControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      masterKegList: []
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -24,7 +25,7 @@ class KegControl extends React.Component {
       currentlyVisibleState = <NewKegForm />
       buttonText = "Return to Keg List";
     } else {
-      currentlyVisibleState = <KegList />
+      currentlyVisibleState = <KegList kegList={this.state.masterKegList} />
       buttonText = "Add Keg";
     }
     return (
