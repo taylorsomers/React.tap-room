e2456bca25e4b947ec0d4ba028ff34787042bd71
Taylor Somers <tsomers@yandex.com>
2020-09-11 12:48:06 -0400

    Refactor to return multiple JSX expressions and hard-code properties for another Keg

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -3,12 +3,20 @@ import Keg from "Keg.js";
 
 function KegList(){
   return (
-    <Keg
-      kegName = "Pumpkin Brew"
-      brewery = "Old Weasel's"
-      price = "$35.00"
-      alcoholContent = "14%"
-      pints = "124" />
+    <React.Fragment>
+      <Keg
+        kegName = "Pumpkin Brew"
+        brewery = "Old Weasel's"
+        price = "$35.00"
+        alcoholContent = "14%"
+        pints = "124" />
+      <Keg
+        kegName = "Snake Bite"
+        brewery = "Wild West Brewery"
+        price = "$45.00"
+        alcoholContent = "11%"
+        pints = "124" />
+    </React.Fragment>
   );
 }
 
