07fbec1352d98f1d351c991db3ea95ee38226cca
Taylor Somers <tsomers@yandex.com>
2020-09-11 13:11:30 -0400

    Add KegControl class and component skeleton

---
+++ src/components/KegControl.js
@@ -0,0 +1,20 @@
+import React from "react";
+import KegList from "./KegList";
+import NewKegForm from "./NewKegForm";
+
+class KegControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        
+      </React.Fragment>
+    );
+  }
+}
+
+export default KegControl;
