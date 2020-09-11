a8cd3b97f06f9028d4d2d7e87d1e7efbd0879f2e
Taylor Somers <tsomers@yandex.com>
2020-09-11 19:38:15 -0400

    Refactor to use ReusableForm

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { v4 } from "uuid";
+import ReusableForm from "./ReusableForm";
 
 function NewKegForm(props){
   function handleNewKegFormSubmission(event) {
@@ -16,30 +17,9 @@ function NewKegForm(props){
   }
   return (
     <React.Fragment>
-      <form onSubmit={handleNewKegFormSubmission}>
-        <input
-          type="text"
-          name="kegName"
-          placeholder="Keg Name" />
-        <input
-          type="text"
-          name="brewery"
-          placeholder="Brewery" />
-        <input
-          type="text"
-          name="price"
-          placeholder="Price" />
-        <input
-          type="number"
-          name="alcoholContent"
-          placeholder="Alcohol Content (Percent)" />
-        <input
-          type="number"
-          name="pints"
-          max="124"
-          placeholder="Number of Pints in Keg" />
-        <button type="submit">Add Keg to Keg List</button>
-      </form>
+      <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Add Keg to Keg List" />
     </React.Fragment>
   )
 }
