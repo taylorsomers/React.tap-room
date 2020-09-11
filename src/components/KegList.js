5c6a83471d346b661d84c7d298cdefbd00be941c
Taylor Somers <tsomers@yandex.com>
2020-09-11 12:57:08 -0400

    Add hard-coded masterKegList and refactor KegList component to map through Kegs in masterKegList

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,21 +1,35 @@
 import React from "react";
 import Keg from "Keg.js";
 
+const masterKegList = [
+  {
+    kegName: "Pumpkin Brew",
+    brewery: "Old Weasel's",
+    price: "$35.00",
+    alcoholContent: 14,
+    pints: 124
+  },
+  {
+    kegName: "Snake Bite",
+    brewery: "Wild West Brewery",
+    price: "$45.00",
+    alcoholContent: 11,
+    pints: 124
+  }
+];
+
 function KegList(){
   return (
     <React.Fragment>
-      <Keg
-        kegName = "Pumpkin Brew"
-        brewery = "Old Weasel's"
-        price = "$35.00"
-        alcoholContent = "14%"
-        pints = "124" />
-      <Keg
-        kegName = "Snake Bite"
-        brewery = "Wild West Brewery"
-        price = "$45.00"
-        alcoholContent = "11%"
-        pints = "124" />
+      {masterKegList.map((keg, index) =>
+        <Keg
+          kegName={keg.kegName}
+          brewery={keg.brewery}
+          price={keg.price}
+          alcoholContent={keg.alcoholContent}
+          pints={keg.pints}
+          key={index} />
+      )}
     </React.Fragment>
   );
 }
