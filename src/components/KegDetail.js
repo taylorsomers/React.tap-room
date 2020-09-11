5fc9a0cfe073685430bea43c0a9b897e5ee0ade9
Taylor Somers <tsomers@yandex.com>
2020-09-11 15:02:14 -0400

    Refactor to receive Keg as prop

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,11 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function KegDetail(props){
+  const { keg } = props;
+
   return (
     <React.Fragment>
-      <h1>Keg Detail</h1>
+      <h1>{keg.kegName}</h1>
+      <h2>{keg.brewery}</h2>
+      <p>{keg.price}</p>
+      <p>{keg.alcoholContent}%</p>
+      <p>{keg.pints} Pints Remaining</p>
     </React.Fragment>
   )
 }
+
+KegDetail.propTypes = {
+  keg: PropTypes.object
+};
 
 export default KegDetail;
