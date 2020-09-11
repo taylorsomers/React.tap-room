38f269f413f5f5adeecf884a4447b26006dab232
Taylor Somers <tsomers@yandex.com>
2020-09-11 13:08:00 -0400

    Correct Header and KegList import directives

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./Header.js";
-import KegList from "./KegList.js";
+import Header from "./Header";
+import KegList from "./KegList";
 
 function App(){
   return (
