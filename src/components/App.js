e0f5bae425ec9c0d59ce4eadf21cac1c3e38dac6
Taylor Somers <tsomers@yandex.com>
2020-09-11 12:40:22 -0400

    Remove hard-coded Keg properties and add KegList to return directive

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import Header from "./Header.js";
+import KegList from "./KegList.js";
 
 function App(){
-  const kegName = "Pumpkin Brew";
-  const brewery = "Old Weasel's";
-  const price = "$35.00";
-  const alcoholContent = "14%";
-  const pints = "124";
   return (
     <React.Fragment>
       <Header />
-      <h1>{kegName}</h1>
-      <h2>{brewery}</h2>
-      <p>{price}</p>
-      <p>{alcoholContent}</p>
-      <p>{pints}</p>
+      <KegList />
     </React.Fragment>
   );
 }
