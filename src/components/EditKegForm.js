442bc0905d72a02ec6d32a686abf8d847387c783
Taylor Somers <tsomers@yandex.com>
2020-09-14 10:15:49 -0400

    Add props to onEditKeg and add formSubmissionHandler to return directive

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -3,9 +3,24 @@ import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
 
 function EditKegForm(props) {
+  const { keg } = props;
+
+  function handleEditKegFormSubmission(event) {
+    event.preventDefault();
+    props.onEditKeg({
+      kegName: event.target.kegName.value,
+      brewery: event.target.brewery.value,
+      price: event.target.price.value,
+      alcoholContent: event.target.alcoholContent.value,
+      pints: event.target.pints.value,
+      id: keg.id()
+    });
+  }
+
   return (
     <React.Fragment>
       <ReusableForm
+        formSubmissionHandler={handleEditKegFormSubmission}
         buttonText="Update Keg"
       />
     </React.Fragment>
