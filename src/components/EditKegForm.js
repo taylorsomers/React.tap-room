35a92157d2940998e87b40fef13ee3b48bb3a8b4
Taylor Somers <tsomers@yandex.com>
2020-09-14 09:59:48 -0400

    Add EditKegForm functional component

---
+++ src/components/EditKegForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+
+function EditKegForm(props) {
+  return (
+    <React.Fragment>
+      <ReusableForm
+        buttonText="Update Keg"
+      />
+    </React.Fragment>
+  );
+}
+
+export default EditKegForm;
